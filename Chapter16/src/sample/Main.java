a0dc923249894d1c611c912d7ed40856bb711ea1
Juan Aguirre <aguirre.juanmp15@gmail.com>
2021-07-23 00:23:50 -0700

    Added the folder for project related to  Chapter16

---
+++ Chapter16/src/sample/Main.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
